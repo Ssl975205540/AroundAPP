6631881b71d50e7ffd53f69211b8d2c9cc8870ba
孙彦飞 <1599782246@qq.com>
2016-10-22 17:39:47 +0800

    网址封装

---
+++ app/src/main/java/lanou/around/tools/recycle/http/URLValues.java
@@ -0,0 +1,24 @@
+package lanou.around.tools.recycle.http;
+
+/**
+ * Created by dllo on 16/10/20.
+ */
+
+public class URLValues {
+
+    //首页页面
+    //你的好友都在这 的网址接口
+    public static final String HOME_FIRENE_SELLING = "http://zhuanzhuan.58.com/zz/transfer/getLatestFriendSelling";
+    //热门市场和轮播图 的网址接口
+    public static final String HOME_HOT_MARKET = "http://zhuanzhuan.58.com/zz/transfer/gettopbanner";
+
+
+    //分类页
+    //手机,数码等图标 的网址接口
+    public static final String CLASSIFY_CHILD_CATES_LOGIC = "http://zhuanzhuan.58.com/zz/transfer/getChildCatesLogic";
+    //手机,数码,电脑,母婴用品等 网址接口
+    public static final String CLASSIFY_WANT_BUY_MESSAGE = "http://zhuanzhuan.58.com/zz/transfer/getIWantBuyCateMessage";
+    //EditText中的图和文字等接口
+    public static final String CLASSIFY_EDITTEXT_TITLTE = "http://zhuanzhuan.58.com/zz/transfer/getTitleInformation";
+
+}
